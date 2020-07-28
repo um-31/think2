47f834996a2fe908e80bf84adda4214f6755acda
Udhay <udaymahajan97@gmail.com>
2020-07-28 02:57:25 -0400

    adding random position to node

--- think2/ViewController.swift
+++ think2/ViewController.swift
@@ -22,12 +22,16 @@ class ViewController: UIViewController {
     
     @IBAction func add(_ sender: Any) {
         let node = SCNNode()
-        node.geometry = SCNBox(width: 0.1, height: 0.1, length: 0.1, chamferRadius: 0)
+        node.geometry = SCNBox(width: 0.1, height: 0.1, length: 0.1, chamferRadius: 0.03)
         node.geometry?.firstMaterial?.diffuse.contents = UIColor.brown
-        node.position = SCNVector3(0,0,1)
+        let x = randomNumber(firstNumber: -0.3, secNumber: 0.3)
+        let y = randomNumber(firstNumber: -0.3, secNumber: 0.3)
+        let z = randomNumber(firstNumber: -0.3, secNumber: 0.3)
+        node.position = SCNVector3(x,y,z)
         self.sceneView.scene.rootNode.addChildNode(node)
     }
     @IBAction func resetSession(_ sender: Any) {
+        self.resset()
         
     }
     
@@ -35,6 +39,9 @@ class ViewController: UIViewController {
         self.sceneView.session.pause()
         self.sceneView.scene.rootNode.enumerateChildNodes { (node,_) in node.removeFromParentNode()}
         self.sceneView.session.run(configuration, options: [.resetTracking,.removeExistingAnchors])
+    }
+    func randomNumber(firstNumber: CGFloat , secNumber: CGFloat ) -> CGFloat {
+        return CGFloat(arc4random()) / CGFloat(UINT32_MAX) * abs(firstNumber - secNumber) + min(firstNumber,secNumber)
     }
     
 }
